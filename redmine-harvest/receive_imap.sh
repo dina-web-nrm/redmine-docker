c6f234e65df2742d460b8db821423f419b85d739
Ingimar E <inkimar@gmail.com>
2017-04-13 13:51:43 +0200

    starting with writing to a file

--- redmine-harvest/receive_imap.sh
+++ redmine-harvest/receive_imap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 source .env
-rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support && echo "Hello $(date)" >>/var/log/cron.log 2>&1
+echo "Mail $(date)" >>/var/log/cron-mail.log 2>&1 && rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support 
