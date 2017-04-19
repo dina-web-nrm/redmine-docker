380d686fec5ddb4ebc0de857510fbad433c4d33e
Ingimar E <inkimar@gmail.com>
2017-04-19 15:13:07 +0200

    adding more log-info

--- redmine-harvest/receive_imap.sh
+++ redmine-harvest/receive_imap.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-source .env
-echo "Mail $(date)" >>/var/log/cron-mail.log 2>&1 && /usr/local/bundle/bin/rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support
+source /usr/src/redmine/mail-script/.env
+echo "Mail, create a task $(date)"   >>/tmp/cron-mail.log 2>&1 
+echo "Mail, echoes the path again  =  $PATH"   >>/tmp/cron-path.log 2>&1 
+rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=inki.dummy@gmail.com password=ingimarnrm project=myproject tracker=Support --trace >> /tmp/cron-mail.log 2>&1
