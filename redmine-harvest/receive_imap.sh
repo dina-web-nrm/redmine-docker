948b45350a6948b3bd42211ccf90d68ea23c10fe
Ingimar E <inkimar@gmail.com>
2017-04-25 15:30:30 +0200

    minor changes, the receive_imap should be run as the root user

--- redmine-harvest/receive_imap.sh
+++ redmine-harvest/receive_imap.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 source /usr/src/redmine/mail-script/.env
-echo "Mail, create a task $(date)"   >>/tmp/cron-mail.log 2>&1 
-echo "Mail, echoes the path again  =  $PATH"   >>/tmp/cron-path.log 2>&1 
-rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=inki.dummy@gmail.com password=ingimarnrm project=myproject tracker=Support --trace >> /tmp/cron-mail.log 2>&1
+echo "Mail 25april klockan 14:52, create a task $(date)"   >>/tmp/cron-mail.log 2>&1 
+/usr/local/bin/bundle exec /usr/local/bundle/bin/rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace >> /tmp/cron-mail.log 2>&1
