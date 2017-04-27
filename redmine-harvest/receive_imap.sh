ab83d06e80f934984d5233e995a10f1cc42ab27d
Ingimar E <inkimar@gmail.com>
2017-04-27 13:43:18 +0200

    this is the script that polls for email, project is hardcoded right now

--- redmine-harvest/receive_imap.sh
+++ redmine-harvest/receive_imap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /usr/src/redmine/mail-script/.env
-echo "Mail 25april klockan 14:52, create a task $(date)"   >>/tmp/cron-mail.log 2>&1 
-/usr/local/bin/bundle exec /usr/local/bundle/bin/rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace >> /tmp/cron-mail.log 2>&1
+echo "Polling for mail fetched $(date)"   >>/tmp/cron-mail.log 2>&1 
+cd /root && bundle exec rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=imap.gmail.com port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace >> /tmp/cron-mail.log 2>&1
