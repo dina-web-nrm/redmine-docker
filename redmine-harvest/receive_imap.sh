d35f2f1bab234899f1f3870e9cd7f5b4e2335790
Ingimar E <inkimar@gmail.com>
2017-08-14 15:40:52 +0200

    removing junk

--- redmine-harvest/receive_imap.sh
+++ redmine-harvest/receive_imap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source /usr/src/redmine/mail-script/.env
-echo "starting"
-cd /root && bundle exec rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=$host port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace  --trace
+echo "starting: for SSL the port should always be 993"
+cd /root && bundle exec rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=$host port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace
 #echo "ending $user and $psw"
