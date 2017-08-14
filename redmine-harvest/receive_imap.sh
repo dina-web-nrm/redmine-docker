575d85abcd0ece9e0ab9162d1de310357b5391dd
Ingimar E <inkimar@gmail.com>
2017-08-14 14:01:58 +0200

    use 993 when using ssl

--- redmine-harvest/receive_imap.sh
+++ redmine-harvest/receive_imap.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source /usr/src/redmine/mail-script/.env
-echo "Polling for mail fetched $(date)"   >>/tmp/cron-mail.log 2>&1 
-cd /root && bundle exec rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=$host port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace >> /tmp/cron-mail.log 2>&1
+echo "starting"
+cd /root && bundle exec rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=$host port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace  --trace
+#echo "ending $user and $psw"
