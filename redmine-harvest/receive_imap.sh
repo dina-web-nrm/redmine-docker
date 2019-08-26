6fd137e2dfb99b459bd356c863692ac49e0cccae
Ingimar Erlingsson <inkimar@gmail.com>
2019-08-26 09:38:24 +0000

    project 'myproject' now 'Myproject'

--- redmine-harvest/receive_imap.sh
+++ redmine-harvest/receive_imap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /usr/src/redmine/mail-script/.env
-echo "starting: for SSL the port should always be 993"
-cd /root && bundle exec rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=$host port=993 ssl=1 username=$user password=$psw project=myproject tracker=Support --trace
+echo "starting"
+echo "Polling for mail fetched $(date)" >>/tmp/cron-mail.log 2>&1 
+cd /root && bundle exec rake -f /usr/src/redmine/Rakefile redmine:email:receive_imap RAILS_ENV=production host=$host port=993 ssl=1 username=$user password=$psw project=Myproject tracker=Support --trace >> /tmp/cron-mail.log 2>&1
 #echo "ending $user and $psw"
